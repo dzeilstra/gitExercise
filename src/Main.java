400e07c0624c988e3e81e707a21b5db3624b1cd1
dzeilstra <alexanderzeilstra2002@gmail.com>
2024-02-16 14:47:28 -0500

    Uh I hope this works

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hey GitHub");
+        System.out.println("It's me, Josephi Krakowski!");
+        System.out.println("Heuristic");
+    }
+}
