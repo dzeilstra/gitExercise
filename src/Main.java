736f9054f6f53be30e17f90a2a6ac94fd70d82c3
dzeilstra <alexanderzeilstra2002@gmail.com>
2024-02-16 14:49:00 -0500

    Uh I hope this works again

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hey GitHub");
         System.out.println("It's me, Josephi Krakowski!");
         System.out.println("Heuristic");
+        System.out.println("Why does everything have to be so difficult for me?");
     }
 }
